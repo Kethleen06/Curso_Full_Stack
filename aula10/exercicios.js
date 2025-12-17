2ff93b031debbc96c5d6237d779753ab073f5d81
Kethleen Gonçalves da Silva <kethleeng06@gmail.com>
2025-12-17 19:01:22 -0300

    tipos de aritméticos

---
+++ aula10/exercicios.js
@@ -0,0 +1,4 @@
+let num1 = 58
+let num2 = 75
+console.log( num1 + num2);
+console.log(typeof num1);
---
+++ aula10/index.js
@@ -0,0 +1,20 @@
+/*
+Aritméticos 
+()
+** potenciação    
+* / % resto da divisão  
+- / + adição / concatenação 
+*/
+const num1 = 5
+const num2 = 10
+console.log(num1 + num2);
+
+/* NaN - Not a number
+ parseInt (inteiro), parseFloat(decimais)
+ number = coloca o valor da variavel em número
+ */
+const num3 = 10;
+const num4 = Number('4.2');
+
+console.log(num3 + num4);
+console.log(typeof num4);
